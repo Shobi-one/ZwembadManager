54848eb81712405c7dad2a3425b8ae5e7e1dccf9
Obi-one <obiverheyen@gmail.com>
2025-12-04 10:31:40 +0100

    Removed unnecessary code

--- ZwembaadManager/Classes/IO.cs
+++ ZwembaadManager/Classes/IO.cs
@@ -48,6 +48,12 @@ namespace ZwembaadManager.Classes
 
             return File.Exists(resourceName) ? File.OpenRead(resourceName) : null;
         }
+        public static string ReadTextResource(string resourceName)
+        {
+            using var stream = GetResourceStream(resourceName);
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
 
         public static T ReadJsonResource<T>(string resourceName)
         {
@@ -56,71 +62,6 @@ namespace ZwembaadManager.Classes
             return JsonSerializer.Deserialize<T>(stream, options)!;
         }
 
-        public static string ReadTextResource(string resourceName)
-        {
-            using var stream = GetResourceStream(resourceName);
-            using var reader = new StreamReader(stream, Encoding.UTF8);
-            return reader.ReadToEnd();
-        }
-
-        public static List<T> ReadCsvResourceAs<T>(string resourceName, char delimiter = ',') where T : new()
-        {
-            var text = ReadTextResource(resourceName);
-            var lines = text.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries);
-
-            if (lines.Length == 0)
-                return [];
-
-            var headers = ParseCsvLine(lines[0], delimiter).ToArray();
-            var properties = GetWritableProperties<T>();
-
-            return lines.Skip(1)
-                       .Select(line => CreateObjectFromCsvLine<T>(line, headers, properties, delimiter))
-                       .ToList();
-        }
-
-        private static PropertyInfo[] GetWritableProperties<T>()
-        {
-            return typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                           .Where(p => p.CanWrite)
-                           .ToArray();
-        }
-
-        private static T CreateObjectFromCsvLine<T>(string line, string[] headers, PropertyInfo[] properties, char delimiter) where T : new()
-        {
-            var fields = ParseCsvLine(line, delimiter).ToArray();
-            var item = new T();
-
-            for (int i = 0; i < Math.Min(headers.Length, fields.Length); i++)
-            {
-                SetPropertyValue(item, headers[i], fields[i], properties);
-            }
-
-            return item;
-        }
-
-        private static void SetPropertyValue<T>(T item, string header, string value, PropertyInfo[] properties)
-        {
-            if (string.IsNullOrWhiteSpace(header) || string.IsNullOrEmpty(value))
-                return;
-
-            var property = properties.FirstOrDefault(p =>
-                string.Equals(p.Name, header, StringComparison.OrdinalIgnoreCase));
-
-            if (property == null)
-                return;
-
-            try
-            {
-                var targetType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
-                var converted = Convert.ChangeType(value, targetType);
-                property.SetValue(item, converted);
-            }
-            catch
-            {
-
-            }
-        }
 
         public static object ReadResource(string resourceName)
         {
@@ -129,73 +70,8 @@ namespace ZwembaadManager.Classes
             {
                 ".json" => JsonDocument.Parse(ReadTextResource(resourceName)).RootElement.Clone(),
                 ".txt" => ReadTextResource(resourceName),
-                ".csv" => ReadCsvAsDictionaries(resourceName),
                 _ => throw new NotSupportedException($"Extension '{extension}' not supported.")
             };
-        }
-
-        private static List<Dictionary<string, string>> ReadCsvAsDictionaries(string resourceName, char delimiter = ',')
-        {
-            var text = ReadTextResource(resourceName);
-            var lines = text.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries);
-
-            if (lines.Length == 0)
-                return [];
-
-            var headers = ParseCsvLine(lines[0], delimiter).ToArray();
-            return lines.Skip(1)
-                       .Select(line => CreateDictionaryFromCsvLine(line, headers, delimiter))
-                       .ToList();
-        }
-
-        private static Dictionary<string, string> CreateDictionaryFromCsvLine(string line, string[] headers, char delimiter)
-        {
-            var fields = ParseCsvLine(line, delimiter).ToArray();
-            var dictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
-            for (int i = 0; i < headers.Length; i++)
-            {
-                dictionary[headers[i]] = i < fields.Length ? fields[i] : string.Empty;
-            }
-
-            return dictionary;
-        }
-
-        private static IEnumerable<string> ParseCsvLine(string line, char delimiter)
-        {
-            if (line is null) yield break;
-
-            var field = new StringBuilder();
-            bool inQuotes = false;
-
-            for (int i = 0; i < line.Length; i++)
-            {
-                var current = line[i];
-
-                if (current == '"')
-                {
-                    if (inQuotes && i + 1 < line.Length && line[i + 1] == '"')
-                    {
-                        field.Append('"');
-                        i++;
-                    }
-                    else
-                    {
-                        inQuotes = !inQuotes;
-                    }
-                }
-                else if (current == delimiter && !inQuotes)
-                {
-                    yield return field.ToString();
-                    field.Clear();
-                }
-                else
-                {
-                    field.Append(current);
-                }
-            }
-
-            yield return field.ToString();
         }
     }
 }
